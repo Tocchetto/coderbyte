a25a16442f6635187c9098a3b1fc0b4ef2412475
Guilherme Zanatta Tocchetto <g.tocchetto@magazineluiza.intranet>
2024-04-17 11:19:27 -0300

    [find_intersection]

---
+++ easy/find_intersection.py
@@ -0,0 +1,31 @@
+"""Find Intersection
+Have the function FindIntersection(strArr) read the array of strings stored in
+strArr which will contain 2 elements: the first element will represent a list
+of comma-separated numbers sorted in ascending order, the second element will
+represent a second list of comma-separated numbers (also sorted). Your goal is
+to return a comma-separated string containing the numbers that occur in
+elements of strArr in sorted order. If there is no intersection, return the
+string false.
+
+Examples:
+Input: ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+Output: 1,4,13
+----
+Input: ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]
+Output: 1,9,10
+"""
+
+def FindIntersection(strArr):
+  list1 = set(map(int, strArr[0].split(", ")))
+  list2 = set(map(int, strArr[1].split(", ")))
+
+  intersection = list1.intersection(list2)
+
+  if not intersection:
+    return "false"
+
+  result = ",".join(map(str, sorted(intersection)))
+  return result
+
+# keep this function call here 
+print(FindIntersection(input()))
